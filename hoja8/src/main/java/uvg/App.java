ef4937015559c5cad276af39371946607659cf37
SebastianUVG <gar22291@uvg.edu.gt>
2024-04-09 18:44:11 -0600

    Lectura del archivo de texto y manipulacion de pacinetes
    Lectura de los archivos de texto a utilizar y forma de manipular los datos de los pacientes segun su prioridad.

--- hoja8/src/main/java/uvg/App.java
+++ hoja8/src/main/java/uvg/App.java
@@ -1,5 +1,11 @@
 package uvg;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,43 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        NewInterface<Sick> bucketList = PriorityQueueFactory.createQueue("java");
+        Scanner sc = new Scanner(System.in);
+        System.out.println( "--------------------Ficha de enfermos--------------------" );
+        System.out.println("Por favor ingrese el nombre del documento de pacientes que quiere abrir (pacientes.txt):");
+        String lista = sc.nextLine();
+        String rutaArchivo = "C:\\Users\\sebas\\OneDrive\\Escritorio\\Github\\EstructuraDeDatos\\HojaDeTrabajo8\\hoja8\\src\\main\\java\\uvg\\"+lista;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
+            String datos;
+            while ((datos = br.readLine()) != null) {
+                String[] partes = datos.split(",");
+                if (partes.length == 3) {
+                    String name = partes[0].trim();
+                    String symptoms = partes[1].trim();
+                    String priority = partes[2].trim();
+                    Sick paciente = new Sick(priority, symptoms, name);
+                    //System.out.println(paciente);
+                    bucketList.add(paciente);
+                }
+            }
+            System.out.println(bucketList);
+        } catch (IOException e) {
+            // TODO: handle exception
+            System.out.println("Error al leer el archivo");
+        }
+
+        
+    }
+
+
+    private static class PriorityQueueFactory{
+        public static NewInterface<Sick> createQueue(String type){
+            if (type.equals("java")) {
+                return new JavaPriorityQueue<Sick>();
+            } else {
+                return new VectorHeap<Sick>();
+            }
+        }
     }
 }
